1da0165cf955ee7117176710fc9b8c6cbdcfa810
Yuta Fujii <yutayutakid21@gmail.com>
2022-06-05 13:31:43 +0900

    コンフリ用

--- TestTest/ViewController.swift
+++ TestTest/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Conflict用")
         // Do any additional setup after loading the view.
     }
 
